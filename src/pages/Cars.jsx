b88eea95b72a3d2418325b44d02b6d5cd9672540
unknown <aliframdani279@gmail.com>
2023-11-10 01:28:30 +0700

    membuat page Cars beserta component-component nya

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,2 +1,12 @@
-function Cars() {}
+import CarsCardFilter from "../components/pages.cars.components/CarsCardFilter";
+import Hero from "../components/pages.landingpage.components/Hero";
+
+function Cars() {
+  return (
+    <>
+      <Hero landingPage={false} />
+      <CarsCardFilter />
+    </>
+  );
+}
 export default Cars;
